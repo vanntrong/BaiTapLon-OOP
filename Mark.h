4dddd1a2320e4032e29bf6e0b06a7527ed42e1e2
vanntrong <vantrongbrv@gmail.com>
2021-10-30 14:14:12 +0700

    them thuoc tinh va phuong thuc cho class mark

--- Mark.h
+++ Mark.h
@@ -1,12 +1,57 @@
 ﻿#include <iostream>
 #include <string>
 #include <math.h>
-#include "People.h"
 
-class Mark : public People
+class Mark
 {
-	private:
-		float dtb;
-		float drl;
-		float dcong;
+private:
+	float GPA;					 //diem trung binh
+	float trainingPoint; //diem ren luyen
+	float extraPoint;		 //diem cong
+public:
+	Mark();
+	Mark(float GPA, float trainingPoint, float extraPoint);
+	~Mark();
+	friend std::istream &operator>>(std::istream &is, Mark &newMark);
+	friend std::ostream &operator<<(std::ostream &os, const Mark &newMark);
+};
+
+// Khai bao khong doi so
+Mark::Mark()
+{
+	this->GPA = this->trainingPoint = this->extraPoint = 0;
+}
+
+// Khai bao co doi so
+Mark::Mark(float GPA, float trainingPoint, float extraPoint)
+{
+	this->GPA = GPA;
+	this->trainingPoint = trainingPoint;
+	this->extraPoint = extraPoint;
+}
+
+//Ham huy
+Mark::~Mark()
+{
+}
+
+//oveloading ham nhap
+std::istream &operator>>(std::istream &is, Mark &newMark)
+{
+	std::cout << "Input GPA: ";
+	is >> newMark.GPA;
+	std::cout << "Input training point: ";
+	is >> newMark.trainingPoint;
+	std::cout << "Input extra point: ";
+	is >> newMark.extraPoint;
+	return is;
+}
+
+//overloading ham xuat
+std::ostream &operator<<(std::ostream &os, const Mark &newMark)
+{
+	os << "GPA: " << newMark.GPA << std::endl;
+	os << "Training point: " << newMark.trainingPoint << std::endl;
+	os << "Extra point: " << newMark.extraPoint << std::endl;
+	return os;
 }
