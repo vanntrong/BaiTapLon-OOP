13e78d6f79d4f8c74621cec957711f05e642e643
vanntrong <vantrongbrv@gmail.com>
2021-10-30 14:35:38 +0700

    them thuoc tinh va phuong thuc cho class People

--- People.h
+++ People.h
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <string>
-#include <math.h>
 #include "Date.h"
 
 using namespace std;
@@ -11,4 +8,40 @@ private:
   string fullName;
   Date dateOfBirth;
   string address;
+
+public:
+  friend std::istream &operator>>(std::istream &is, People &newPeople);
+  friend std::ostream &operator<<(std::ostream &os, const People &newPeople);
 };
+
+//overloading ham nhap
+std::istream &operator>>(std::istream &is, People &newPeople)
+{
+  std::cout << std::endl
+            << "**********Input**********" << std::endl
+            << std::endl;
+  std::cout << "Input full name: ";
+  fflush(stdin);
+  getline(is, newPeople.fullName);
+  cout << "Input date of birth: " << std::endl;
+  std::cin >> newPeople.dateOfBirth;
+  std::cout << "Input address: ";
+  fflush(stdin);
+  getline(is, newPeople.address);
+  std::cout << std::endl
+            << "*************************" << std::endl;
+  return is;
+}
+
+std::ostream &operator<<(std::ostream &os, const People &newPeople)
+{
+  os << std::endl
+     << "**********Information**********" << std::endl
+     << std::endl;
+  os << "Full name: " << newPeople.fullName << std::endl;
+  os << "Date of birth: " << std::endl;
+  std::cout << newPeople.dateOfBirth;
+  os << "Address: " << newPeople.dateOfBirth << std::endl;
+  os << "*******************************" << std::endl;
+  return os;
+}
