8a097e92ad195b59a0957b759663c660e722ac26
vanntrong <vantrongbrv@gmail.com>
2021-12-10 18:35:47 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
-  Yêu cầu: 1. Thêm chức năng kiểm tra mssv bị trùng
-           2. Thêm chức năng tìm kiếm bằng chữ thường
+  Yêu cầu: 1. Thêm chức năng kiểm tra mssv bị trùng: Thêm dòng 598-614, 65-83, 111-127 file menu.h
+           2. Thêm chức năng tìm kiếm bằng chữ thường: Thêm dòng 337-352 file menu.h
   - Võ Văn Trọng: 6151071109
   - Bùi Nhật Huy:
   - Nguyễn Thiên Kha:
